fbb4139d20521421c3bf6f8683fa3c42ab0f70ed
Roman Lunin <240654@vutbr.cz>
2025-10-29 10:18:41 +0100

    edit

---
+++ cv6/cv6/Core/Inc/sct.h
@@ -0,0 +1,20 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 29, 2025
+ *      Author: 240654
+ */
+
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include "main.h"
+
+void sct_init(void);
+
+void sct_led(uint32_t value);
+
+void sct_value(uint16_t value, uint8_t led);
+
+#endif /* INC_SCT_H_ */
